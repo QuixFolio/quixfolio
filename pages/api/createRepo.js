a0c0288a792b2bef3920cf694ddcd307bbfc794a
Parth Shah <helloparthshah@gmail.com>
2023-03-21 09:20:26 -0700

    Dynamic modification

--- pages/api/createRepo.js
+++ pages/api/createRepo.js
@@ -93,112 +93,40 @@ export default async function handler(req, res) {
                         })
                 }
             }
-
-            function changeContent(id, page, newContent) {
-                const $ = load(pages[page].content)
-                $(`#${id}`).text(newContent)
-                pages[page].content = $.html()
-            }
-
-            changeContent("name", data.pages["name"], req.body.name)
-            changeContent("summary", data.pages["summary"], req.body.summary)
-            let $ = load(pages[data.pages["education"]].content)
-
-            let educationDiv = $("#education").children().first().clone()
-            $("#education").children().remove()
-            for (let education of req.body.education) {
-                let edu = educationDiv.clone()
-                edu.find("[university]").text(education.university)
-                edu.find("[start-year]").text(education.startYear)
-                edu.find("[end-year]").text(education.endYear)
-                edu.find("[major]").text(education.major)
-                edu.find("[gpa]").text(education.gpa)
-                edu.find("[add-info]").text(education.addInfo)
-                edu.find("[relevent-coursework]").text(education.releventCoursework)
-                $("#education").append(edu)
-            }
-            pages[data.pages["education"]].content = $.html()
-
-            $ = load(pages[data.pages["work-experience"]].content)
-            let workExperienceDiv = $("#work-experience").children().first().clone()
-            $("#work-experience").children().remove()
-            for (let workExperience of req.body.workExperience) {
-                let work = workExperienceDiv.clone()
-                work.find("[company]").text(workExperience.company)
-                work.find("[location]").text(workExperience.location)
-                work.find("[position]").text(workExperience.position)
-                work.find("[start-date]").text(workExperience.startDate)
-                work.find("[end-date]").text(workExperience.endDate)
-                work.find("[summary]").text(workExperience.summary)
-                work.find("[image]").attr("src", workExperience.image)
-                $("#work-experience").append(work)
-            }
-            pages[data.pages["work-experience"]].content = $.html()
-
-            $ = load(pages[data.pages["publication"]].content)
-            let publicationDiv = $("#publication").children().first().clone()
-            $("#publication").children().remove()
-            for (let publication of req.body.publication) {
-                let pub = publicationDiv.clone()
-                pub.find("[title]").text(publication.title)
-                pub.find("[authors]").text(publication.authors)
-                pub.find("[publisher]").text(publication.publisher)
-                pub.find("[date]").text(publication.date)
-                pub.find("[url]").text(publication.url)
-                pub.find("[summary]").text(publication.summary)
-                $("#publication").append(pub)
-            }
-            pages[data.pages["publication"]].content = $.html()
-
-            $ = load(pages[data.pages["projects"]].content)
-            let projectsDiv = $("#projects").children().first().clone()
-            $("#projects").children().remove()
-            for (let project of req.body.projects) {
-                let proj = projectsDiv.clone()
-                proj.find("[name]").text(project.name)
-                proj.find("[description]").text(project.description)
-                proj.find("[source]").attr("href", project.source)
-                proj.find("[demo]").attr("href", project.demo)
-                proj.find("[image]").attr("src", project.image)
-                proj.find("[start-date]").text(project.startDate)
-                proj.find("[end-date]").text(project.endDate)
-                $("#projects").append(proj)
-            }
-            pages[data.pages["projects"]].content = $.html()
-
-            $ = load(pages[data.pages["skills"]].content)
-            let skillsDiv = $("#skills").children().first().clone()
-            $("#skills").children().remove()
-            for (let skill of req.body.skills) {
-                let sk = skillsDiv.clone()
-                sk.find("[skills]").text(skill.skills)
-                $("#skills").append(sk)
-            }
-            pages[data.pages["skills"]].content = $.html()
-
-            $ = load(pages[data.pages["achievements"]].content)
-            let achievementsDiv = $("#achievements").children().first().clone()
-            $("#achievements").children().remove()
-            for (let achievement of req.body.achievements) {
-                let ach = achievementsDiv.clone()
-                ach.find("[name]").text(achievement.name)
-                ach.find("[date]").text(achievement.date)
-                ach.find("[position]").text(achievement.position)
-                $("#achievements").append(ach)
-            }
-            pages[data.pages["achievements"]].content = $.html()
-
-            $ = load(pages[data.pages["links"]].content)
-            let linksDiv = $("#links").children().first().clone()
-            $("#links").children().remove()
-            for (let link of req.body.links) {
-                let lnk = linksDiv.clone()
-                lnk.find("[link]").text(link.name)
-                lnk.find("[link]").attr("href", link.link)
-                $("#links").append(lnk)
-            }
-            pages[data.pages["links"]].content = $.html()
-
+            Object.keys(req.body).forEach(key => {
+                let ele = key.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2').toLowerCase()
+                if (Array.isArray(req.body[key])) {
+                    if (req.body[key].length === 0) return
+                    let $ = load(pages[data.pages[ele]].content)
+                    let div = $(`#${ele}`).children().first().clone()
+                    $(`#${ele}`).children().remove()
+                    for (let item of req.body[key]) {
+                        let el = div.clone()
+                        Object.keys(item).forEach(k => {
+                            let field = k.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2').toLowerCase()
+                            // check if the element is an image or a link
+                            if (el.find(`[${k}]`).is("img")) {
+                                el.find(`[${k}]`).attr("src", item[k])
+                                return
+                            } else if (el.find(`[${k}]`).is("a")) {
+                                el.find(`[${k}]`).attr("href", item[k])
+                                return
+                            }
+                            el.find(`[${field}]`).text(item[k])
+                        })
+                        $(`#${ele}`).append(el)
+                    }
+                    pages[data.pages[ele]].content = $.html()
+                } else {
+                    try {
+                        let $ = load(pages[data.pages[ele]].content)
+                        $(`#${ele}`).text(req.body[key])
+                        pages[data.pages[ele]].content = $.html()
+                    } catch (error) {
+                    }
+                }
+            })
+            let $ = load(pages["index.html"].content)
             console.log($.html())
 
             await Promise.all(Object.keys(pages).map(async page => {
