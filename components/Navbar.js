cd5b346de070740b76fc1c148e08b301dd9e97a0
Parth Shah <helloparthshah@gmail.com>
2023-04-02 15:57:21 -0700

    added click to home for logo

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,6 +4,7 @@ import GitHub from "./github";
 import MenuIcon from '@mui/icons-material/Menu';
 import Image from "next/image";
 import logo from "../public/QuixFolio.png";
+import Link from "next/link";
 export default function Navbar() {
 
     return (
@@ -19,16 +20,16 @@ export default function Navbar() {
                     >
                         <MenuIcon />
                     </IconButton> */}
-                    <Box sx={{ flexGrow: 1 }} >
-                        <Image
-                            src={logo}
-                            alt="QuixFolio"
-                            height={40}
-                        />
+                    <Box sx={{ flexGrow: 1 }}>
+                        <Link href="/">
+                            <Image
+                                src={logo}
+                                alt="QuixFolio"
+                                height={45}
+                                href="/"
+                            />
+                        </Link>
                     </Box>
-                    {/* <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        QuixFolio
-                    </Typography> */}
                     <GitHub />
                 </Toolbar>
             </AppBar>
